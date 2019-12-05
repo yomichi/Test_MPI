42613a3cdd9677ad0793437fcf10cb5403a45c0d
Yuichi Motoyama <yomichi@tsg.jp>
2019-12-05 12:51:47 +0900

    update cmake template

--- cmake_template/test.c
+++ cmake_template/test.c
@@ -3,10 +3,10 @@
 
 int main(int argc, char *argv[])
 {
-    int rank; //rank of the process
-    int size; //number of processes
+    int rank;
+    int size;
 
-    MPI_Init(&argc,&argv); //inititate MPI environment
+    MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
     MPI_Comm_size(MPI_COMM_WORLD,&size);
 
