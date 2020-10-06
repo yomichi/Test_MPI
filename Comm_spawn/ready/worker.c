5d51be774eb42d616b9107c0df4531b096855fac
Yuichi Motoyama <yomichi@tsg.jp>
2020-10-06 22:38:46 +0900

    add indent

--- Comm_spawn/ready/worker.c
+++ Comm_spawn/ready/worker.c
@@ -12,11 +12,11 @@ int main(int argc, char **argv)
   MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  printf("I'm %d of %d\n", rank, nprocs);
+  printf("  I'm %d of %d\n", rank, nprocs);
 
   MPI_Comm_get_parent(&parent);
   if(parent != MPI_COMM_NULL){
-    printf("I'm spawned by MPI_Comm_spawn\n");
+    printf("  I'm spawned by MPI_Comm_spawn\n");
     MPI_Send(&ret, 1, MPI_INT, 0, 0, parent);
   }
 
